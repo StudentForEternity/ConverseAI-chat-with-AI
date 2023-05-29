48fcd36ee5ca3ccc120e90c8e9e8d252f7295459
StudentForEternity <109116999+StudentForEternity@users.noreply.github.com>
2023-05-28 20:11:38 -0400

    adding database

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+import { initializeApp } from "firebase/app"
+import { getDatabase, ref, push, get } from "firebase/database"
 import { Configuration, OpenAIApi } from "openai"
 import { process } from "./env"
+
+const appSettings = {
+  databaseURL: "",
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const conversationInDatabase = ref(database)
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
-
 const openai = new OpenAIApi(configuration)
 const chatbotConversation = document.getElementById("chatbot-conversation")
 
-const conversationArr = [
-  {
-    role: "system",
-    content: `You are a highly sarcastic assistant that gives short answers.`,
-  },
-]
+const instructionsObj = {
+  role: "system",
+  content: `You are a highly sarcastic assistant that gives short answers.`,
+}
 
 document.addEventListener("submit", (e) => {
   e.preventDefault()
   const userInput = document.getElementById("user-input")
-
-  conversationArr.push({
+  push(conversationInDatabase, {
     role: "user",
     content: userInput.value,
   })
 
-  fetchReply(conversationArr)
-  console.log(conversationArr)
+  fetchReply()
 
   const newSpeechBubble = document.createElement("div")
   newSpeechBubble.classList.add("speech", "speech-human")
@@ -35,17 +39,26 @@ document.addEventListener("submit", (e) => {
   chatbotConversation.scrollTop = chatbotConversation.scrollHeight
 })
 
-async function fetchReply() {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: conversationArr,
-    presence_penalty: 2,
-    frequency_penalty: 0.2,
+function fetchReply() {
+  get(conversationInDatabase).then(async (snapshot) => {
+    if (snapshot.exists()) {
+      const conversationArr = Object.values(snapshot.val())
+      conversationArr.unshift(instructionsObj)
+
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: conversationArr,
+        presence_penalty: 1,
+        frequency_penalty: 0.2,
+      })
+
+      console.log(response.data.choices[0].message)
+      push(conversationInDatabase, response.data.choices[0].message)
+      renderTypewriterText(response.data.choices[0].message.content)
+    } else {
+      console.log("No data available in the Database...")
+    }
   })
-
-  conversationArr.push(response.data.choices[0].message)
-
-  renderTypewriterText(response.data.choices[0].message.content)
 }
 
 function renderTypewriterText(text) {
